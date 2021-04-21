bf1e6f7b6ca846382b42fabd949de0d696def299
Bartłomiej Suchecki <suchyba.bq@gmail.com>
2021-04-21 14:31:29 +0200

    Created Web project for .NET backend.

---
+++ GroupBy.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GroupBy.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
