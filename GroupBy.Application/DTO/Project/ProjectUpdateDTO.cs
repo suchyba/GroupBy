41e145f99be202aa74f2116597b72ebd6f6cc1d4
Bartłomiej Suchecki <suchyba.bq@gmail.com>
2021-06-28 15:58:21 +0200

    #5 Created repository and DTOs for Project entity.

---
+++ GroupBy.Application/DTO/Project/ProjectUpdateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy.Application.DTO.Project
+{
+    public class ProjectUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? BeginDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool Active { get; set; }
+        public bool Independent { get; set; }
+        public int ProjectGroupId { get; set; }
+        public int ParentGroupId { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
---
+++ GroupBy.Application/Validators/Project/ProjectValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using GroupBy.Application.DTO.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy.Application.Validators.Project
+{
+    public class ProjectValidator : AbstractValidator<ProjectDTO>
+    {
+        public ProjectValidator()
+        {
+
+        }
+    }
+}
