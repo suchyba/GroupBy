ee87f8c0fda353e5a10ddaf32e3d5f3e8f9b21de
Bartłomiej Suchecki <47833851+suchyba@users.noreply.github.com>
2026-06-06 01:01:05 +0200

    Restrict inventory book delete (#134)
    Added restriction on inventory book delation

---
+++ GroupBy.Infrastructure/Migrations/20260605225439_RestrictDeleteInventoryBook.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroupBy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestrictDeleteInventoryBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InventoryBookRecords_InventoryBooks_InventoryBookId",
+                table: "InventoryBookRecords");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryBookRecords_InventoryBooks_InventoryBookId",
+                table: "InventoryBookRecords",
+                column: "InventoryBookId",
+                principalTable: "InventoryBooks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InventoryBookRecords_InventoryBooks_InventoryBookId",
+                table: "InventoryBookRecords");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryBookRecords_InventoryBooks_InventoryBookId",
+                table: "InventoryBookRecords",
+                column: "InventoryBookId",
+                principalTable: "InventoryBooks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
